5689f620d2de59a6495fc3fac0d27cd3e40e803a
Mihai Floares <psecto@macbookmihai.home>
2018-06-13 00:13:40 +0200

    git push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,27 @@ export class AppComponent implements OnInit
 
 {
   MyTitle: string;
-  
+ 
+ 
   ngOnInit(): void {
   let quotes = [
         'yo',
         'punk',
         'whatsup'
   ];
+    let color = [
+      "#FFFF00",
+      "#000000",
+      "#FF0000"
+    ]
+
   let index = 0;
   this.MyTitle = quotes[index];
   setInterval(() => { index=(index+1)%3; this.MyTitle = quotes[index]; }, 3000);
+  this.myColor = color[index];
+  setInterval(() => { index=(index+1)%3; this.myColor = color[index]; }, 3000);
 
-
-
-
+ 
 
   throw new Error("Method not implemented.");
   }
