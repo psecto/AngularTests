45dfe46eaecbe5d5d0fa67538784de3c976624a3
Mihai Floares <psecto@macbookmihai.home>
2018-06-13 08:34:14 +0200

    Lisible code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,11 +26,13 @@ export class AppComponent implements OnInit
 
   let index = 0;
   this.MyTitle = quotes[index];
-  setInterval(() => { index=(index+1)%3; this.MyTitle = quotes[index]; }, 3000);
   this.myColor = color[index];
-  setInterval(() => { index=(index+1)%3; this.myColor = color[index]; }, 3000);
-
- 
+  setInterval(() => 
+  { 
+    index=(index+1)%3; 
+    this.MyTitle = quotes[index]; 
+    this.myColor = color[index];
+  }, 3000);
 
   throw new Error(" KO DUUDE not implemented.");
   }
